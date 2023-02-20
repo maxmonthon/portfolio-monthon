6950ad658e77076c8db260129c6de573b6884208
maxmonthon <70101075+maxmonthon@users.noreply.github.com>
2023-02-20 18:01:05 +0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -58,16 +58,7 @@ export default function Header() {
           fgColor="gray"
           bgColor="transparent"
         />
-        <SocialIcon
-          url="https://www.linkedin.com/in/monthon-kachana-10b096267/"
-          fgColor="gray"
-          bgColor="transparent"
-        />
-            <SocialIcon
-          url="https://www.linkedin.com/in/monthon-kachana-10b096267/"
-          fgColor="gray"
-          bgColor="transparent"
-        />
+    
       </motion.div>
 
       {/* --------------------------------------------------------------------------------- */}
